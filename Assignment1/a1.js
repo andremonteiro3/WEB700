d34b5bf6bb970955d58658fe63ce76afbd9a6d57
Andre Hideo Onoda Monteiro <amonteiro3@myseneca.ca>
2024-05-23 18:14:37 -0400

    Add comment Headers

--- Assignment1/a1.js
+++ Assignment1/a1.js
@@ -1,4 +1,12 @@
-
+/*******************************************************************************
+*  WEB700 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Andre Hideo Onoda Monteiro Student ID: 101947232 Date: 05/23/2024
+*
+********************************************************************************/ 
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
