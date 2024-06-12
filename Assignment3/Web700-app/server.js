70fe66a1883b572a330634a71a7aa2764c3d74bb
Andre Hideo Onoda Monteiro <amonteiro3@myseneca.ca>
2024-06-11 20:52:01 -0400

    Add header to server.js

--- Assignment3/Web700-app/server.js
+++ Assignment3/Web700-app/server.js
@@ -1,3 +1,12 @@
+/*********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Andre Hideo Onoda Monteiro Student ID: 101947232 Date: 06/11/2024
+*
+********************************************************************************/ 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var path = require("path");
