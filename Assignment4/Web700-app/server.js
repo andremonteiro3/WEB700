30c85fb15494f10b100f4ecf7ec95a36711de406
Andre Hideo Onoda Monteiro <amonteiro3@myseneca.ca>
2024-07-15 18:33:10 -0400

    Assignment 4

--- Assignment4/Web700-app/server.js
+++ Assignment4/Web700-app/server.js
@@ -1,18 +1,21 @@
 /*********************************************************************************
-*  WEB700 – Assignment 03
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
-*  of this assignment has been copied manually or electronically from any other source 
-*  (including 3rd party web sites) or distributed to other students.
-* 
-*  Name: Andre Hideo Onoda Monteiro Student ID: 101947232 Date: 06/11/2024
-*
-********************************************************************************/ 
+ *  WEB700 – Assignment 04
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part
+ *  of this assignment has been copied manually or electronically from any other source
+ *  (including 3rd party web sites) or distributed to other students.
+ *
+ *  Name: Andre Hideo Onoda Monteiro Student ID: 101947232 Date: 06/11/2024
+ *
+ ********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var path = require("path");
 
 var dataCollection = require("./modules/collegeData");
 var app = express();
+
+app.use(express.static("./public/"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/students", (req, res) => {
   if (req.query.course) {
@@ -27,6 +30,20 @@ app.get("/students", (req, res) => {
       .catch((err) => res.status(500).send({ message: "no results" }));
   }
 });
+
+app.get("/students/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "addStudent.html"));
+});
+
+app.post("/students/add", (req, res) => {
+  dataCollection
+    .addStudent(req.body)
+    .then((v) => res.status(200).send({ studentNum: v }))
+    .catch((err) =>
+      res.status(500).send({ message: "Couldn't register student." }),
+    );
+});
+
 app.get("/students/:studentId", (req, res) => {
   dataCollection
     .getStudentByNum(req.params.studentId)
@@ -56,9 +73,6 @@ app.get("/about", (req, res) => {
 app.get("/htmlDemo", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "htmlDemo.html"));
 });
-app.get("/404.css", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "404.css"));
-});
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "views", "404.html"));
@@ -68,7 +82,7 @@ dataCollection
   .initialize()
   .then(() =>
     app.listen(HTTP_PORT, () =>
-      console.log("Server listening on port: " + HTTP_PORT)
-    )
+      console.log("Server listening on port: " + HTTP_PORT),
+    ),
   )
   .catch((err) => console.log(err));
