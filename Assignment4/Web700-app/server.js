b13618b1549adf929e691ed0c692698c7731b76a
Andre Hideo Onoda Monteiro <amonteiro3@myseneca.ca>
2024-07-15 11:25:22 -0400

    Create assignment4 base

---
+++ Assignment4/Web700-app/server.js
@@ -0,0 +1,74 @@
+/*********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Andre Hideo Onoda Monteiro Student ID: 101947232 Date: 06/11/2024
+*
+********************************************************************************/ 
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var path = require("path");
+
+var dataCollection = require("./modules/collegeData");
+var app = express();
+
+app.get("/students", (req, res) => {
+  if (req.query.course) {
+    dataCollection
+      .getStudentsByCourse(req.query.course)
+      .then((result) => res.send(result))
+      .catch((err) => res.status(500).send({ message: "no results" }));
+  } else {
+    dataCollection
+      .getAllStudents()
+      .then((result) => res.send(result))
+      .catch((err) => res.status(500).send({ message: "no results" }));
+  }
+});
+app.get("/students/:studentId", (req, res) => {
+  dataCollection
+    .getStudentByNum(req.params.studentId)
+    .then((result) => res.send(result))
+    .catch((err) => res.status(500).send({ message: "no results" }));
+});
+
+app.get("/tas", (req, res) => {
+  dataCollection
+    .getTAs()
+    .then((result) => res.send(result))
+    .catch((err) => res.status(500).send({ message: "no results" }));
+});
+
+app.get("/courses", (req, res) => {
+  dataCollection
+    .getCourses()
+    .then((result) => res.send(result))
+    .catch((err) => res.status(500).send({ message: "no results" }));
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+app.get("/htmlDemo", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "htmlDemo.html"));
+});
+app.get("/404.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "404.css"));
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+dataCollection
+  .initialize()
+  .then(() =>
+    app.listen(HTTP_PORT, () =>
+      console.log("Server listening on port: " + HTTP_PORT)
+    )
+  )
+  .catch((err) => console.log(err));
